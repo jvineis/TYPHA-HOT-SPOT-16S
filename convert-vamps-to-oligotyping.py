c1f5c5632d0784c1b5c22ea3798044f0700ed9c6
Joseph Henry Vineis <jvineis@gmail.com>
2024-08-15 11:10:29 -0400

    Add files via upload

--- convert-vamps-to-oligotyping.py
+++ convert-vamps-to-oligotyping.py
@@ -3,13 +3,20 @@ import sys
 
 outfile = open(sys.argv[2],'w')
 dat = open(sys.argv[1], 'r')
+sample_names = []
+count = 0
+sample_dict = {}
+for i in open(sys.argv[3],'r'):
+    x = i.strip()
+    sample_dict[count] = x
+    count += 1
+
 for line in dat:
     x = line.strip().split('\t')
 #    print(x[12])
-    sample_count_list = []
     snum = 11
     for i in range(snum):
-
         for ele in range(int(x[i])):
-            print(i,ele,x[12])
-            outfile.write(">sample-"+str(i)+"_Read"+str(ele)+'\n'+x[12]+'\n')
+#            print(i,ele,x[12])
+            print(sample_dict[i])
+            outfile.write(">"+sample_dict[i]+"_Read"+str(ele)+'\n'+x[12]+'\n')
